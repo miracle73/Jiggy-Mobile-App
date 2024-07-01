7e9e944585d7f609c44bf80a1397dc1b3f922e1d
miracle73 <nwadiaromiraclechukwuma@gmail.com>
2024-07-01 15:30:44 +0200

    Recent changes

--- BottomNavigationScreens/Home.tsx
+++ BottomNavigationScreens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StyleSheet, Image } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Image, ImageBackground } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { EvilIcons, AntDesign, Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
@@ -6,6 +6,9 @@ import Union from '../assets/image/Best.png'
 import Picture from '../assets/image/Picture.png'
 import Message from '../assets/image/message.png'
 import Best from '../assets/image/Best2.png'
+import Icon from '../assets/image/Best3.png'
+import Background from '../assets/image/post.png'
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Home = () => {
   return (
@@ -35,16 +38,13 @@ const Home = () => {
         <Image source={Union} style={{}} />
         <Text style={styles.thirdText}>Anonymous Boosted this post</Text>
       </View>
-      <Text style={[styles.fourthText, {paddingHorizontal: 10, marginTop: 5}]}>Have a great day with my amazing client all the way
+      <Text style={[styles.fourthText, { paddingHorizontal: 10, marginTop: 5 }]}>Have a great day with my amazing client all the way
         from NewYork</Text>
       <View style={styles.firstContainer}>
         <View style={{
           flexDirection: 'row',
           justifyContent: 'space-between',
           alignItems: 'center',
-          // gap: 8,
-
-
         }}>
           <View style={[styles.innerNavigation, { justifyContent: 'flex-start', gap: 4 }]}>
 
@@ -71,14 +71,14 @@ const Home = () => {
         }}>
           <Text style={styles.seventhText}>DM</Text>
         </View>
-        <View style={[styles.innerNavigation, { width: '100%', }]}>
-        
+        <View style={[styles.innerNavigation, { width: '100%' }]}>
+
           <View style={[styles.simpleContainer, { gap: 4, borderRightWidth: 1, borderRightColor: '#C4C4C4' }]}>
             <Entypo name="arrow-bold-up" size={25} color="#F33F5E" />
             <Text style={[styles.eighthText, { marginLeft: 2 }]}>803</Text>
-            <MaterialCommunityIcons name="arrow-down-bold-outline" size={25} color="#C4C4C4" style={{ marginRight: 8}} />
+            <MaterialCommunityIcons name="arrow-down-bold-outline" size={25} color="#C4C4C4" style={{ marginRight: 8 }} />
           </View>
-          <View style={[styles.simpleContainer, {gap: 4, borderRightWidth: 1, borderRightColor: '#C4C4C4' }]}>
+          <View style={[styles.simpleContainer, { gap: 4, borderRightWidth: 1, borderRightColor: '#C4C4C4' }]}>
             <Image source={Message} />
             <Text style={[styles.eighthText, { color: "#FFFFFF", marginRight: 8 }]}>4682</Text>
           </View>
@@ -86,11 +86,71 @@ const Home = () => {
             <Image source={Best} />
             <Text style={[styles.eighthText, { color: "#FFFFFF" }]}>20</Text>
           </View>
-          <AntDesign name="upload" size={25} color="#FFFFFF" style={{ marginLeft: 15 }}/>
+          <AntDesign name="upload" size={25} color="#FFFFFF" style={{ marginLeft: 15 }} />
         </View>
-      
-      </View>
 
+      </View>
+      <View style={styles.secondContainer}>
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          gap: 8,
+          paddingTop: 10
+        }}>
+          <Image source={Union} style={{}} />
+          <Text style={styles.thirdText}>Anonymous Boosted this post</Text>
+        </View>
+
+        <ImageBackground
+          source={Background}
+          style={styles.thirdContainer}
+          imageStyle={{ borderRadius: 7 }}
+        >
+
+          <View style={[styles.innerNavigation, { width: '100%', justifyContent: 'center', gap: 4, }]}>
+
+            <View style={{ height: 26, width: 26, borderRadius: 13 }}>
+              <Image source={Picture} />
+            </View>
+            <View>
+              <Text style={styles.fifthText}>Anonymous</Text>
+              <Text style={styles.thirdText}>12h ago . IMSU</Text>
+            </View>
+
+          </View>
+          <Text
+            style={{
+              color: '#E3E3E3',
+              fontWeight: "700",
+              fontSize: 14,
+              textAlign: 'center',
+              marginTop: 20
+            }}
+          >
+            I was so horny
+          </Text>
+
+        </ImageBackground>
+        <View style={[styles.innerNavigation, { width: '100%', marginBottom: 10 }]}>
+
+          <View style={[styles.simpleContainer, { gap: 4, borderRightWidth: 1, borderRightColor: '#C4C4C4' }]}>
+
+            <MaterialCommunityIcons name="arrow-up-bold-outline" size={25} color="#C4C4C4"  />
+            <Text style={[styles.eighthText, { marginLeft: 2, color: '#B20000' }]}>803</Text>
+            <Entypo name="arrow-bold-down" size={25} color="#B20000" />
+          </View>
+          <View style={[styles.simpleContainer, { gap: 4, borderRightWidth: 1, borderRightColor: '#C4C4C4' }]}>
+            <Image source={Message} />
+            <Text style={[styles.eighthText, { color: "#FFFFFF", marginRight: 8 }]}>4682</Text>
+          </View>
+          <View style={styles.simpleContainer}>
+            <Image source={Icon} />
+            <Text style={[styles.eighthText, { color: "#FFFFFF" }]}>20</Text>
+          </View>
+          <AntDesign name="upload" size={25} color="#FFFFFF" style={{ marginLeft: 15 }} />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
@@ -143,7 +203,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontWeight: "500",
     fontSize: 11,
-    
+
   },
   eighthText: {
     color: '#F33F5E',
@@ -171,7 +231,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    
-  }
+
+  },
+  secondContainer: {
+    borderBottomWidth: 4,
+    borderTopWidth: 4,
+    borderColor: '#1E1E1E',
+    paddingHorizontal: 10
+
+  },
+  thirdContainer: {
+    height: 150,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 10,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    paddingTop: 10
+  },
 })
 export default Home
